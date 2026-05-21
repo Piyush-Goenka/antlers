f72c7ebb77df6431d98eae657f2dd3c98f7ac3f4
maedi <maediprichard@gmail.com>
2026-05-22 00:29:35 +1000

    Prefix with renderable

--- lib/nodes/prop_node.rb
+++ lib/nodes/prop_node.rb
@@ -11,13 +11,13 @@ module Antlers
       props = evaluate_props(props: @props, current_binding:)
       event = create_render_event(props:)
 
-      klass = class_from_namespace(namespace: namespace&.split('::') || [], name: @name)
-      instance = klass.new(event:)
+      renderable_klass = class_from_namespace(namespace: namespace&.split('::') || [], name: @name)
+      renderable_instance = renderable_klass.new(event:)
 
       # Classes referenced via "<{ ChildNode }>" must implement class/instance render/render_template methods (See LowNode).
-      return instance.render_template(event:, parent_binding:, props:) if klass.template
+      return renderable_instance.render_template(event:, parent_binding:, props:) if renderable_klass.template
 
-      props.empty? ? instance.render(event:) : instance.render(event:, **props)
+      props.empty? ? renderable_instance.render(event:) : renderable_instance.render(event:, **props)
     end
   end
 end
