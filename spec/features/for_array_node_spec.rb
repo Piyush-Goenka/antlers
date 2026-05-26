2de0271f8ce0f97dab9ef36244420bf6ed90a428
maedi <maediprichard@gmail.com>
2026-05-26 11:15:02 +0200

    Rename for node to for array node

---
+++ spec/features/for_array_node_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'lowload'
+require 'low_node'
+
+require_relative '../../lib/nodes/for_node'
+
+LowLoad.lowload('spec/fixtures/for_array_node.rbx')
+
+RSpec.describe RBX::ForArrayNode do
+  subject(:for_node) { described_class }
+
+  let(:event) { 'mock event' }
+
+  describe '<{ for: item in: @items }>' do
+    it 'renders items' do
+      expect(RBX::ForArrayNode.render(event:).response.body.read).to eq(
+        <<~HTML.delete(" \n")
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+        HTML
+      )
+    end
+  end
+end
